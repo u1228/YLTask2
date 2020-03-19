f20cb79a05e37f7af901259fd645a2c45fc714f2
u1228 <58815441+u1228@users.noreply.github.com>
2020-03-19 15:40:32 +0300

    Add files via upload

--- data/users.py
+++ data/users.py
@@ -2,6 +2,7 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from .db_session import SqlAlchemyBase, create_session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
@@ -20,3 +21,10 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, 
                                       default=datetime.datetime.now)
+    
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
